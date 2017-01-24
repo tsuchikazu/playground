7f1539404c16ad7ce5d56f5dded12d3fb4ce24ba
tsuchikazu <huneyhunt77@gmail.com>
2017-01-25 00:45:11 +0900

    ch04. neural network learning(two_layer_net)

--- neural_network_leaning/simpleNet.py
+++ neural_network_leaning/simpleNet.py
@@ -18,17 +18,20 @@ def cross_entropy_error(y, t):
         y = y.reshape(1, y.size)
 
     batch_size = y.shape[0]
-    # return -np.sum(np.log(y[np.arange(batch_size), t])) / batch_size
-    delta = 1e-7
-    return -np.sum(t * np.log(y + delta)) / batch_size
+    return -np.sum(np.log(y[np.arange(batch_size), t])) / batch_size
+
+    # delta = 1e-7
+    # return -np.sum(t * np.log(y + delta)) / batch_size
 
 def numerical_gradient(f, x):
     h = 1e-4 # 0.0001
     grad = np.zeros_like(x)
 
-    for idx in range(x.size):
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
         tmp_val = x[idx]
-        x[idx] = tmp_val + h
+        x[idx] = float(tmp_val) + h
         fxh1 = f(x) # f(x+h)
 
         x[idx] = tmp_val - h
@@ -36,8 +39,26 @@ def numerical_gradient(f, x):
         grad[idx] = (fxh1 - fxh2) / (2*h)
 
         x[idx] = tmp_val # 値を元に戻す
+        it.iternext()
 
     return grad
+# 本の実装と違うけど↑にすると動く
+# def numerical_gradient(f, x):
+#     h = 1e-4 # 0.0001
+#     grad = np.zeros_like(x)
+#
+#     for idx in range(x.size):
+#         tmp_val = x[idx]
+#         x[idx] = tmp_val + h
+#         fxh1 = f(x) # f(x+h)
+#
+#         x[idx] = tmp_val - h
+#         fxh2 = f(x) # f(x-h)
+#         grad[idx] = (fxh1 - fxh2) / (2*h)
+#
+#         x[idx] = tmp_val # 値を元に戻す
+#
+#     return grad
 
 class simpleNet:
     def __init__(self):
@@ -56,7 +77,6 @@ class simpleNet:
 
 net = simpleNet()
 
-print(net.W)
 x = np.array([0.6, 0.9])
 p = net.predict(x)
 print(p)
@@ -64,8 +84,8 @@ print(p)
 t = np.array([0, 0, 1])
 print(net.loss(x, t))
 
-# def f(W):
-#     return net.loss(x,t)
-# dW = numerical_gradient(f, net.W)
-# print(dW)
+def f(W):
+    return net.loss(x, t)
+dW = numerical_gradient(f, net.W)
+print(dW)
 
