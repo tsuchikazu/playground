187e399f8323d7a60e4a51f46d4b8963fae45ff8
tsuchikazu <huneyhunt77@gmail.com>
2017-03-07 00:53:20 +0900

    reimplement Deep MNIST for Export by keras

--- tensorflow/Deep_MNIST_for_Experts.py
+++ tensorflow/Deep_MNIST_for_Experts.py
@@ -29,27 +29,29 @@ def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
                           strides=[1, 2, 2, 1], padding='SAME')
 
-# 5×5の 1channel を 32channel へ
+# 5×5のフィルタで 1channel を 32channel へ
 W_conv1 = weight_variable([5, 5, 1, 32])
 b_conv1 = bias_variable([32])
+# 28×28の1channelに変換
 x_image = tf.reshape(x, [-1,28,28,1])
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
 
+# 5×5のフィルタで 32channel を 64channelへ
 W_conv2 = weight_variable([5, 5, 32, 64])
 b_conv2 = bias_variable([64])
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 
 
-
+# 28(元サイズ)/2(pool)/2(pool) = 7×7の64channelになっている。7*7*64=3136 を 1024へ
 W_fc1 = weight_variable([7 * 7 * 64, 1024])
 b_fc1 = bias_variable([1024])
 h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
-
+# over fitting防止
 keep_prob = tf.placeholder(tf.float32)
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
@@ -67,7 +69,7 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 sess = tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
 
-for i in range(2000):
+for i in range(500):
     batch = mnist.train.next_batch(50)
     if i%100 == 0:
         train_accuracy = accuracy.eval(feed_dict={
